15a171a6068c8732fa1d41b48f958cfe846f84e1
GakmanNN <gnn87@mail.ru>
2023-05-04 21:15:02 +0300

    arrow functions

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,80 @@
 import './style.scss'
 import './style.css'
+import 'lite-youtube-embed/src/lite-yt-embed.css'
+import 'lite-youtube-embed/src/lite-yt-embed.js'
 
+let value=10
+console.log(value)
+
+// При описании функции мы указываем параметры, которые ожидаем получить как аргументы при вызове
+const showMessage = function(n:number, a = ' Я значение по умолчанию аргумента "а"') {
+  let local = 6
+  let value = 5
+  console.log('локальная переменная вместо глобальной',value)
+  console.log(local)
+  // console.log(n)
+  n++
+  console.log('Всем привет!' + a + ' ' + local + ' ' + n)
+  return n
+}
+
+// При вызове функции в неё передаются аргументы
+showMessage(value,' Я аргумент "a"')
+console.log('Возвращаемое значение функции', showMessage(value))
+console.log(value)
+// showMessage()
+
+// Мы объявляем функции со списком параметров, затем вызываем их, передавая аргументы
+
+// Несколько вариантов возврата значения по условию
+function checkAge(age:number) {
+  return age>18||confirm('Родители разрешили?')
+  return (age > 18) ? true : confirm('Родители разрешили?')
+  if (age > 18) {
+    return true;
+  } else {
+    return confirm('Родители разрешили?');
+  }
+}
+
+console.log(checkAge)
+
+function ask(question: string, yes: Function, no: Function) {
+  if (confirm(question)) yes()
+  else no();
+}
+// Анонимные функции-колбэки
+// ask(
+//   "Вы согласны?",
+//   function () { alert("Вы согласились."); },
+//   function () { alert("Вы отменили выполнение."); }
+// )
+
+// ask(
+//   "Вы согласны?",
+//   () => alert('Вы согласились.') :
+//   () => alert("Вы отменили выполнение.")
+// )
+
+
+// Стрелочная функция
+// let func = (arg1, arg2, ...argN) => expression
+// let func = function (arg1, arg2, ...argN) {
+//   return expression
+// }
+
+// Отсутствие фигурных скобок после стрелки, НЕЯВНО (не пишем return) возвращает результат
+let func1 = (a: number, b: number) => a+b;
+// let func1 = (a: number, b: number) => { return a }
+let func2 = function (a: number, b: number) {
+  return a+b
+}
+// Стрелочная функция с логикой и явным возвратом
+let func3 = (a: number, b: number) => { 
+  const c = a + b
+  return c 
+}
+
+console.log('func1', func1(1,2))
+console.log('func2', func2(1,2))
+console.log('func3', func3(1,2))
